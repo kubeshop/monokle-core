2edc935642d84a1a15c38024a871e086b5c3e2f1
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-10-26 17:25:32 +0300

    refactor: remove unused

--- packages/components/src/molecules/ActivityBar/ActivityBarItem.tsx
+++ packages/components/src/molecules/ActivityBar/ActivityBarItem.tsx
@@ -2,7 +2,7 @@ import Colors from "@/styles/Colors";
 import { Tooltip } from "antd";
 import { useCallback } from "react";
 
-import { ActivityBarItemProps, ActivityType } from "./types";
+import { ActivityBarItemProps } from "./types";
 
 import * as S from "./ActivityBarItem.styled";
 
