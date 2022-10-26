bb43dfdabfdb92752aea5caf0b2e9db2f5f44ec2
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-10-26 10:00:40 +0300

    feat: add more stories for ResizableColumnsPanel

--- packages/components/src/molecules/Panels/ResizableColumnsPanel.stories.tsx
+++ packages/components/src/molecules/Panels/ResizableColumnsPanel.stories.tsx
@@ -1,27 +1,38 @@
-import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import ResizableColumnsPanel from "./ResizableColumnsPanel";
 
-const meta: ComponentMeta<typeof ResizableColumnsPanel> = {
+export default {
   title: "Molecules/ResizableColumnsPanel",
   component: ResizableColumnsPanel,
-};
-export default meta;
+} as ComponentMeta<typeof ResizableColumnsPanel>;
 
-export const Primary: ComponentStoryObj<typeof ResizableColumnsPanel> = {
-  args: {
-    left: <div style={{ background: "blue", height: "100%" }}>Left Panel</div>,
-    center: <div style={{ background: "yellow", height: "100%" }}>Center Panel</div>,
-    right: <div style={{ background: "red", height: "100%" }}>Right Panel</div>,
-    height: 500,
-    width: 1500,
-    layout: {
-      left: 0.3,
-      center: 0.2,
-      right: 0.5,
-    },
-    onStopResize: (position, flex) => {
-      console.log("Position:", position);
-      console.log("Flex:", flex);
-    },
+const Template: ComponentStory<typeof ResizableColumnsPanel> = (args) => <ResizableColumnsPanel {...args} />;
+
+export const AllPanels = Template.bind({});
+AllPanels.args = {
+  left: <div style={{ background: "blue", height: "100%" }}>Left Panel</div>,
+  center: <div style={{ background: "yellow", height: "100%" }}>Center Panel</div>,
+  right: <div style={{ background: "red", height: "100%" }}>Right Panel</div>,
+  height: 500,
+  width: 1500,
+  layout: {
+    left: 0.3,
+    center: 0.2,
+    right: 0.5,
   },
+  onStopResize: (position, flex) => {},
+};
+
+export const WithoutLeftPanel = Template.bind({});
+WithoutLeftPanel.args = {
+  center: <div style={{ background: "yellow", height: "100%" }}>Center Panel</div>,
+  right: <div style={{ background: "red", height: "100%" }}>Right Panel</div>,
+  height: 500,
+  width: 1500,
+  layout: {
+    left: 0.3,
+    center: 0.3,
+    right: 0.4,
+  },
+  onStopResize: (position, flex) => {},
 };
