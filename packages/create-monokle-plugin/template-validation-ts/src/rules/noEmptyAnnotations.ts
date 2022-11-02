5cab59c424d5547024f0719d3ae2726d09fff851
Delnat Wito <wito.delnat@gmail.com>
2022-11-02 17:21:20 +0100

    feat: add create-monokle-plugin package

---
+++ packages/create-monokle-plugin/template-validation-ts/src/rules/noEmptyAnnotations.ts
@@ -0,0 +1,19 @@
+import { defineRule } from "@monokle/validation/custom";
+
+export const noEmptyAnnotations = defineRule({
+  id: "RCA001",
+  description: "Require annotations as metadata.",
+  help: "Add any annotation to the Kubernetes resource.",
+  validate({ resources }, { report }) {
+    resources.forEach((resource) => {
+      const annotations = Object.entries(
+        resource.content.metadata?.annotations ?? {}
+      );
+      const hasAnnotations = annotations.length > 0;
+
+      if (!hasAnnotations) {
+        report(resource, { path: "metadata.annotations" });
+      }
+    });
+  },
+});
