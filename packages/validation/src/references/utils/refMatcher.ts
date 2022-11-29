b0667886bfdd44b9865650f34e61b6212a1d09ee
Delnat Wito <wito.delnat@gmail.com>
2022-11-29 14:41:01 +0100

    fix: fix cyclic dependency

---
+++ packages/validation/src/references/utils/refMatcher.ts
@@ -0,0 +1,9 @@
+import type { RefMapper } from "../mappers/mappers.js";
+
+export function refMapperMatchesKind(refMapper: RefMapper, kind: string) {
+  if (kind && refMapper.target.kind.startsWith("$")) {
+    return kind.match(refMapper.target.kind.substring(1)) !== null;
+  }
+
+  return refMapper.target.kind === kind;
+}
