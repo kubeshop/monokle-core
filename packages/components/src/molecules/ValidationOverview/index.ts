ff011c88d62fab88926cefb9344ab76315149899
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-03-21 16:08:07 +0200

    refactor: rename

--- packages/components/src/molecules/ValidationOverview/index.ts
+++ packages/components/src/molecules/ValidationOverview/index.ts
@@ -1,2 +1,1 @@
-export {default as ValidationOverview} from './ValidationOverview';
-export type {ValidationOverviewType} from './types';
+export {default as ValidationOverivew} from './ValidationOverview';
--- packages/components/src/molecules/index.ts
+++ packages/components/src/molecules/index.ts
@@ -8,6 +8,5 @@ export * from './TitleBar';
 export * from './TitleBarCount';
 export * from './ValidationOverview';
 export * from './ValidationPopover';
-export * from './VirtualizedValidation';
 export * from './WalkThrough';
 export * from './WalkThroughCard';
