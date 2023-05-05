0e21524f608bfe26d2f99c69b6cac574b7b52e39
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-05-05 15:09:20 +0300

    fix: pane close icon

--- packages/components/src/molecules/AllotmentPanels/AllotmentResizableColumnsPanel.tsx
+++ packages/components/src/molecules/AllotmentPanels/AllotmentResizableColumnsPanel.tsx
@@ -12,44 +12,57 @@ const AllotmentResizableColumnsPanel: React.FC<AllotmentResizableColumnsPanelTyp
   const {center, left, right, minPaneWidth = 350, leftClosable = false, paneCloseIconStyle = {}, defaultSizes} = props;
   const {onDragEnd = () => {}, onCloseLeftPane = () => {}} = props;
 
-  console.log('Center:', center);
-  console.log('Left:', left);
-  console.log('Right:', right);
-
   return (
-    <Allotment onDragEnd={onDragEnd} defaultSizes={defaultSizes} proportionalLayout={false}>
-      <Allotment.Pane visible={Boolean(left)} minSize={minPaneWidth} preferredSize={defaultSizes?.[0]}>
-        <LeftPane $leftClosable={leftClosable}>
-          {left}
-          {leftClosable && (
-            <PaneCloseIcon
-              onClick={onCloseLeftPane}
-              containerStyle={{
-                position: 'absolute',
-                top: 18,
-                right: -8,
-                zIndex: LAYOUT.zIndex.high,
-                ...paneCloseIconStyle,
-              }}
-            />
-          )}
-        </LeftPane>
-      </Allotment.Pane>
+    <Container $leftClosable={leftClosable}>
+      <Allotment onDragEnd={onDragEnd} defaultSizes={defaultSizes} proportionalLayout={false}>
+        <Allotment.Pane visible={Boolean(left)} minSize={minPaneWidth} preferredSize={defaultSizes?.[0]}>
+          <LeftPane $leftClosable={leftClosable}>
+            {left}
+            {leftClosable && (
+              <PaneCloseIcon
+                onClick={onCloseLeftPane}
+                containerStyle={{
+                  position: 'absolute',
+                  top: 18,
+                  right: -8,
+                  zIndex: LAYOUT.zIndex.high,
+                  ...paneCloseIconStyle,
+                }}
+              />
+            )}
+          </LeftPane>
+        </Allotment.Pane>
 
-      <Allotment.Pane minSize={minPaneWidth} visible={Boolean(center)} priority={LayoutPriority.Low}>
-        <Pane>{center}</Pane>
-      </Allotment.Pane>
+        <Allotment.Pane minSize={minPaneWidth} visible={Boolean(center)} priority={LayoutPriority.Low}>
+          <Pane>{center}</Pane>
+        </Allotment.Pane>
 
-      <Allotment.Pane minSize={minPaneWidth} priority={LayoutPriority.High}>
-        <Pane>{right}</Pane>
-      </Allotment.Pane>
-    </Allotment>
+        <Allotment.Pane minSize={minPaneWidth} priority={LayoutPriority.High}>
+          <Pane>{right}</Pane>
+        </Allotment.Pane>
+      </Allotment>
+    </Container>
   );
 };
 
 export default AllotmentResizableColumnsPanel;
 
 // Styled Components
+
+const Container = styled.div<{$leftClosable: boolean}>`
+  height: 100%;
+  width: 100%;
+
+  ${({$leftClosable}) => {
+    if ($leftClosable) {
+      return `
+        .split-view-view-visible:first-child {
+          overflow: visible;
+        }
+      `;
+    }
+  }}
+`;
 
 const Pane = styled.div`
   position: relative;
