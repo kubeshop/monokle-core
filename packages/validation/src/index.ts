03146853baebd504a1a2b92ba1164214321777c2
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-06-23 13:42:34 +0300

    fix validation export

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -10,3 +10,6 @@ export * from './createDefaultMonokleValidator.browser.js';
 export * from './config/index.browser.js';
 export * from './config/read.browser.js';
 export * from './wasmLoader/RemoteWasmLoader.browser.js';
+
+// This function is used in Node environments only, but it cannot be imported in Monokle Desktop if it's not defined here as well
+export const fetchBundleRequireCustomPlugin = async () => {};
