a94063bc36bd6b93d2ebe1a06471ff6183b10335
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-08-01 13:38:52 +0300

    refactor: problem info configure rule cta

--- packages/components/src/molecules/ProblemInfo/ProblemInfo.tsx
+++ packages/components/src/molecules/ProblemInfo/ProblemInfo.tsx
@@ -14,7 +14,7 @@ import {SecurityFrameworkTag} from '../ValidationOverview/ProblemRenderer';
 
 const ProblemInfo: React.FC<ProblemInfoType> = props => {
   const {containerClassName = '', containerStyle = {}, problem, rule} = props;
-  const {onSettingsClick, onHelpURLClick, onLocationClick} = props;
+  const {onConfigureRule, onHelpURLClick, onLocationClick} = props;
 
   const errorLocation = useMemo(() => getFileLocation(problem), [problem]);
   const title = useMemo(
@@ -53,8 +53,13 @@ const ProblemInfo: React.FC<ProblemInfoType> = props => {
             : null}
         </IconsContainer>
 
-        <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="Rule setup">
-          <Button type="link" icon={<SettingOutlined />} onClick={onSettingsClick} />
+        <Tooltip
+          mouseEnterDelay={TOOLTIP_DELAY}
+          title="Enable / disable rule, change priority, access all the rules in this plugin."
+        >
+          <Button type="link" icon={<SettingOutlined />} onClick={onConfigureRule}>
+            Configure rule
+          </Button>
         </Tooltip>
       </TitleContainer>
 
