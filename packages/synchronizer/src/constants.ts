0a3aa536cb66ae57e08d250cb8c398d717345a25
f1ames <dr.odpowiedz@gmail.com>
2023-08-16 14:44:52 +0200

    fix(synchronizer): extend device flow handler configurability and update its defaults

--- packages/synchronizer/src/constants.ts
+++ packages/synchronizer/src/constants.ts
@@ -3,5 +3,8 @@ export const DEFAULT_STORAGE_CONFIG_FILE_AUTH = 'auth.yaml';
 
 export const DEFAULT_API_URL = 'https://api.monokle.com';
 
-export const DEFAULT_DEVICE_FLOW_CLIENT_ID = 'monokle';
-export const DEFAULT_DEVICE_FLOW_IDP_URL = 'https://api.dev.monokle.com/identity';
+export const DEFAULT_DEVICE_FLOW_IDP_URL = 'https://id.dev.monokle.com/realms/monokle';
+export const DEFAULT_DEVICE_FLOW_CLIENT_ID = 'mc-cli';
+export const DEFAULT_DEVICE_FLOW_CLIENT_SECRET = '';
+export const DEFAULT_DEVICE_FLOW_ALG = 'ES512';
+export const DEFAULT_DEVICE_FLOW_CLIENT_SCOPE = 'openid profile offline_access';
