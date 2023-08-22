9f92ffcfc06d3bfaa76b48bfd667ac0c813a3e5b
f1ames <dr.odpowiedz@gmail.com>
2023-08-22 12:05:09 +0200

    feat(synchronizer): update device flow default configuration

--- packages/synchronizer/src/constants.ts
+++ packages/synchronizer/src/constants.ts
@@ -3,7 +3,7 @@ export const DEFAULT_STORAGE_CONFIG_FILE_AUTH = 'auth.yaml';
 
 export const DEFAULT_API_URL = 'https://api.monokle.com';
 
-export const DEFAULT_DEVICE_FLOW_IDP_URL = 'https://id.dev.monokle.com/realms/monokle';
+export const DEFAULT_DEVICE_FLOW_IDP_URL = 'https://id.monokle.com/realms/monokle';
 export const DEFAULT_DEVICE_FLOW_CLIENT_ID = 'mc-cli';
 export const DEFAULT_DEVICE_FLOW_CLIENT_SECRET = '';
 export const DEFAULT_DEVICE_FLOW_ALG = 'ES512';
