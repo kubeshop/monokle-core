e4cbf2808f653c30ce173e4165dc745840d3ea9d
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-08-22 14:51:31 +0300

    refactor: use webpack to build synchronizer as CJS

---
+++ packages/synchronizer/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      'node:os': 'os',
+      'node:path': 'path',
+      'node:process': 'process',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
