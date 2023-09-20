e9cd8fa90f6761ea2ae0f25193e40fadd624572c
Delnat Wito <wito.delnat@gmail.com>
2023-09-20 11:50:09 +0200

    fix(validation): PSS strict capabilities

--- packages/validation/src/validators/pod-security-standards/rules/PSS206-capabilities-strict.ts
+++ packages/validation/src/validators/pod-security-standards/rules/PSS206-capabilities-strict.ts
@@ -2,7 +2,7 @@ import {PSS_RELATIONS} from '../../../taxonomies/index.js';
 import {defineRule} from '../../custom/config.js';
 import {validatePodSpec} from '../../custom/utils.js';
 
-const ALLOWED_DROP = 'ALL'; // Any list of capabilities that includes ALL
+const REQUIRED_DROP = 'ALL'; // Any list of capabilities that includes ALL
 const ALLOWED_ADD = ['NET_BIND_SERVICE'];
 
 export const capabilitiesStrict = defineRule({
@@ -18,15 +18,15 @@ export const capabilitiesStrict = defineRule({
   validate({resources}, {report}) {
     validatePodSpec(resources, (resource, pod, prefix) => {
       pod.initContainers?.forEach((container, index) => {
-        const validDrop = container.securityContext?.capabilities?.drop?.some(c => c === ALLOWED_DROP) ?? false;
-        if (validDrop) {
+        const validDrop = container.securityContext?.capabilities?.drop?.some(c => c === REQUIRED_DROP) ?? false;
+        if (!validDrop) {
           report(resource, {
             path: `${prefix}.initContainers.${index}.securityContext.capabilities.drop`,
           });
         }
 
         const validAdd = container.securityContext?.capabilities?.add?.every(c => ALLOWED_ADD.includes(c));
-        if (validAdd) {
+        if (!validAdd) {
           report(resource, {
             path: `${prefix}.initContainers.${index}.securityContext.capabilities.add`,
           });
@@ -34,15 +34,15 @@ export const capabilitiesStrict = defineRule({
       });
 
       pod.ephemeralContainers?.forEach((container, index) => {
-        const validDrop = container.securityContext?.capabilities?.drop?.some(c => c === ALLOWED_DROP) ?? false;
-        if (validDrop) {
+        const validDrop = container.securityContext?.capabilities?.drop?.some(c => c === REQUIRED_DROP) ?? false;
+        if (!validDrop) {
           report(resource, {
             path: `${prefix}.ephemeralContainers.${index}.securityContext.capabilities.drop`,
           });
         }
 
         const validAdd = container.securityContext?.capabilities?.add?.every(c => ALLOWED_ADD.includes(c));
-        if (validAdd) {
+        if (!validAdd) {
           report(resource, {
             path: `${prefix}.ephemeralContainers.${index}.securityContext.capabilities.add`,
           });
@@ -50,15 +50,15 @@ export const capabilitiesStrict = defineRule({
       });
 
       pod.containers.forEach((container, index) => {
-        const validDrop = container.securityContext?.capabilities?.drop?.some(c => c === ALLOWED_DROP) ?? false;
-        if (validDrop) {
+        const validDrop = container.securityContext?.capabilities?.drop?.some(c => c === REQUIRED_DROP) ?? false;
+        if (!validDrop) {
           report(resource, {
             path: `${prefix}.containers.${index}.securityContext.capabilities.drop`,
           });
         }
 
         const validAdd = container.securityContext?.capabilities?.add?.every(c => ALLOWED_ADD.includes(c));
-        if (validAdd) {
+        if (!validAdd) {
           report(resource, {
             path: `${prefix}.containers.${index}.securityContext.capabilities.add`,
           });
