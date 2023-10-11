7f6f3c2b12d1f9017ab944e85f43ba8bf8c871a5
Delnat Wito <wito.delnat@gmail.com>
2023-10-11 12:16:11 +0200

    fix: fix no pod create/execute rules

--- packages/validation/src/validators/practices/rules/KBP107-noPodCreate.ts
+++ packages/validation/src/validators/practices/rules/KBP107-noPodCreate.ts
@@ -4,6 +4,8 @@ import {isClusterRole} from '../../custom/schemas/clusterrole.rbac.authorization
 import {isRole} from '../../custom/schemas/role.rbac.authorization.k8s.io.v1.js';
 import {NSA_RELATIONS} from '../../../taxonomies/nsa.js';
 
+const KIND_CREATES_PODS = ['pods', 'deployments', 'statefulsets', 'jobs', 'cronjobs', 'daemonsets', 'replicasets'];
+
 export const noPodCreate = defineRule({
   id: 107,
   description: 'Disallow permissions to create pods',
@@ -15,8 +17,10 @@ export const noPodCreate = defineRule({
   validate({resources}, {report}) {
     resources.filter(isTarget).forEach((role, index) => {
       role.rules?.forEach(rule => {
-        const isValid = rule.verbs?.includes('create');
-        if (isValid) return;
+        const createPermission = rule.verbs?.includes('create');
+        const resourceCanCreatePod = rule.resources?.some(r => KIND_CREATES_PODS.includes(r.toLowerCase()));
+        const isInvalid = resourceCanCreatePod && createPermission;
+        if (!isInvalid) return;
         report(role, {path: `spec.rules.${index}.verbs`});
       });
     });
