107f9b1458e05ecf516aabe098c672a1040e4633
f1ames <dr.odpowiedz@gmail.com>
2023-11-16 11:49:22 +0100

    refactor: add method to get default storage paths for more flexibility

--- packages/synchronizer/src/handlers/storageHandlerPolicy.ts
+++ packages/synchronizer/src/handlers/storageHandlerPolicy.ts
@@ -1,13 +1,11 @@
-import envPaths from 'env-paths';
 import {Document} from 'yaml';
-import {StorageHandler} from './storageHandler.js';
-import {DEFAULT_STORAGE_CONFIG_FOLDER} from '../constants.js';
+import {StorageHandler, getDefaultStorageConfigPaths} from './storageHandler.js';
 import type {ValidationConfig} from '@monokle/types';
 
 export type StoragePolicyFormat = ValidationConfig;
 
 export class StorageHandlerPolicy extends StorageHandler<StoragePolicyFormat> {
-  constructor(storageFolderPath: string = envPaths(DEFAULT_STORAGE_CONFIG_FOLDER, {suffix: ''}).cache) {
+  constructor(storageFolderPath: string = getDefaultStorageConfigPaths().cache) {
     super(storageFolderPath);
   }
 
